d47ea7d5f5016c1011a95b5fa3f90ba857a9998f
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-11-07 05:31:08 +0000

    Remove unused code that was commented out

--- timer2.js
+++ timer2.js
@@ -35,17 +35,3 @@ const handleUserInput = (key) => {
 
 // Event listener for keyboard input
 stdin.on("data", handleUserInput);
-
-
-/*const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question('What do you think of Node.js? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-
-  rl.close();
-});*/
