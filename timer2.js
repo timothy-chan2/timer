003a4a0808789f74bd485863d78aeb0e10e82061
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-11-07 05:27:08 +0000

    Add interactivity by keyboard input

---
+++ timer2.js
@@ -0,0 +1,51 @@
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.setEncoding("utf8");
+stdin.resume();
+
+const handleUserInput = (key) => {
+  // Beep when b is pressed
+  if (key === 'b') {
+    process.stdout.write('\x07');
+  }
+  
+  switch(key) {
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+      console.log(`Setting timer for ${key} seconds...`);
+      setTimeout(() => {
+        process.stdout.write('\x07');
+      }, key * 1000);
+      break;
+  }
+
+  // Escape out of program with Ctrl + C (close stdin connection)
+  if (key === '\u0003') {
+    process.stdout.write('Thanks for using me, ciao!\n');
+    process.exit();
+  }
+};  
+
+// Event listener for keyboard input
+stdin.on("data", handleUserInput);
+
+
+/*const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What do you think of Node.js? ', (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.close();
+});*/
